ce1873bef8423377d26521100abda28562b51d31
Boluo85 <3082259166@qq.com>
2018-06-11 18:54:19 +0800

    change5

--- drag.js
+++ drag.js
@@ -1,6 +1,8 @@
-window.onload =function(){
-
-}
-function() {
-    
+function drag(id) {
+    var obj = document.getElementById(id);
+    var disX = 0;
+    var disY = 0;
+    obj.onmousedown =function () {
+        
+    }
 }
